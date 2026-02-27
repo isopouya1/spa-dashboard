12cb16b815100d0c50203f6194f0e64dbf0564d3
isopouya <pouya2402@gmail.com>
2026-02-27 23:09:50 +0330

    add push and popstate to project for routes

--- public/spa.js
+++ public/spa.js
@@ -1,4 +1,4 @@
-function router() {
+function router(params) {
   const routes = [
     { path: "/", view: () => console.log("home") },
     { path: "/products", view: () => console.log("products") },
@@ -14,10 +14,31 @@ function router() {
     }
   })
   const matchRoute = potentialRoute.find((route) => route.isMatch)
+
+  if (!matchRoute) {
+    matchRoute = {
+        route : {path : '/not-found', view : () => console.log('not found')},
+        isMatch : true,
+    }
+  }
+
   matchRoute.route.view()
   
 }
 
+function navigateTo(url){
+    history.pushState(null,null,url)
+    router()
+}
+
+window.addEventListener("popstate" , router)
+
 document.addEventListener("DOMContentLoaded" , () => {
+    document.body.addEventListener("click" , (e) => {
+        if (e.target.matches('[data-link]')) {
+            e.preventDefault()
+            navigateTo(e.target.href)
+        }    
+    })
     router()
 })
