d342730283e5c443342b8a127976a46f3d1484b3
isopouya <pouya2402@gmail.com>
2026-02-27 22:55:19 +0330

    add route structour

--- public/spa.js
+++ public/spa.js
@@ -1,3 +1,23 @@
-function Router(){
-    
+function router() {
+  const routes = [
+    { path: "/", view: () => console.log("home") },
+    { path: "/products", view: () => console.log("products") },
+    { path: "/courses", view: () => console.log("course") },
+    { path: "/users", view: () => console.log("users") },
+  ];
+
+
+  const potentialRoute = routes.map((route) => {
+    return {
+        route: route , 
+        isMatch : location.pathname == route.path
+    }
+  })
+  const matchRoute = potentialRoute.find((route) => route.isMatch)
+  matchRoute.route.view()
+  
 }
+
+document.addEventListener("DOMContentLoaded" , () => {
+    router()
+})
